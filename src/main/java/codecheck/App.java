14b686ffad67f64ff0bab3fbb6a4c12c1984d0ce
tie303922 <tie303922@S1711C12619-T1.tisnt.tis.co.jp>
2018-06-22 10:36:05 +0900

    コミット

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -4,7 +4,7 @@ public class App {
 	public static void main(String[] args) {
 		for (int i = 0, l = args.length; i < l; i++) {
 			if(i == "A") {
-				int a = ;0
+				int a = 0;
 				System.out.println(a);
 			}
 		}
